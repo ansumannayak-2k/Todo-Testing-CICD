d331cc09df83c890763693e5d54a5d54bcad6321
Ansuman Nayak <ansumannayak800@gmail.com>
2025-04-07 18:07:28 +0530

    Updated locust file with endpoint

--- locustfile.py
+++ locustfile.py
@@ -5,4 +5,4 @@ class WebsiteUser(HttpUser):
 
     @task
     def load_test(self):
-        self.client.get("course/devops-and-cloud-computing-course-JAN-Batch/67763fb09c7eb6fec2f20ecf/lesson/67ee6fa074ee33dbc16f2ca3/?lectureType=video&milestoneId=677651c79c7eb6fec2f23d69&sectionId=678b75ae2b28919111f6b55c")
+        self.client.get("learn/career-path/devops-and-cloud-computing-course-JAN-Batch/67763fb09c7eb6fec2f20ecf/dashboard/?from=/learn/&clickText=continue&page_from=my_course_page")
